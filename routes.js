598427c1ac10030308d127e60b8f9627d28f5f98
Alister <Alister@AlisterMBP.local>
2015-02-22 00:16:03 -0500

    sup1

--- routes.js
+++ routes.js
@@ -14,25 +14,29 @@ router.get('/project/:slug', function(req, res){
 });
 
 
-var Flickr = require("flickrapi"),
-    flickrOptions = {
-      api_key: "ebd88a37617f947edcbe35c153c7179a",
-      secret: "f6b24a5974345704"
-    };
- 
-Flickr.tokenOnly(flickrOptions, function(error, flickr) {
-  // we can now use "flickr" as our API object,
-  // but we can only call public methods and access public data
-  console.log(flickr);
-});
+
 
 
 // Index
 router.get('/', function(req, res) {
   
-  res.render('index', {data: result});
+  var Flickr = require("flickrapi"),
+    flickrOptions = {
+      api_key: "ebd88a37617f947edcbe35c153c7179a",
+      secret: "f6b24a5974345704"
+    };
+ 
+    Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+      // we can now use "flickr" as our API object,
+      // but we can only call public methods and access public data
 
+      flickr.photos.getInfo({
+        photo_id: 15985618334        
+      }, function(err, result) {
+        res.render('index', {data: result});
+      });
 
+    });
 });
 
 module.exports = router;
